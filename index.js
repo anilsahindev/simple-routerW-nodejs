c5408ea58aef4db5ab1053afcda4ea92556f279d
Anıl Şahin <anlshn@anlshn.com>
2018-04-12 13:08:03 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+    response.writeHead(200, { 'content-type': 'text/html; charset=utf-8' });
+
+    if (request.method === "GET"){
+        if (request.url === '/')
+            response.write("İndex");
+        else if (request.url === 'contact')
+            response.write("contact page");
+        else
+            response.write("Not Found");
+    }
+
+    response.end();
+});
+
+server.listen(3000);
